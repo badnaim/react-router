7c042ae97adbd1c5f65355c81ed5469f070bdb2b
badnaim <batnyaam.tumurbat@gmail.com>
2023-01-15 20:29:05 +0800

    b

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from "../components/subcomponent/NavBar"
+
+export default function Header() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
---
+++ src/util/data.js
@@ -0,0 +1,24 @@
+export const MENUS = [
+  { name: "HOME", url: "/" },
+  { name: "ABOUT", url: "/about" },
+  { name: "LOGIN", url: "/login" },
+];
+
+export const users = [
+  {
+    userName: "bat",
+    password: "12",
+  },
+  {
+    userName: "bold",
+    password: "123",
+  },
+  {
+    userName: "od",
+    password: "1234",
+  },
+  {
+    userName: "mod",
+    password: "12345",
+  },
+];
